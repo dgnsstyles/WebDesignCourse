7f88e8f535a13a005104c6ad1be4fa4e86688044
David Goren <davidgoren@MacBook-Pro-de-David.local>
2018-06-22 23:41:03 -0400

    agregado parte de javascript

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+// alert('el javascript funciona');
+/* tipos de Datos:
+  Numeros
+  Strings
+  Arrays
+  Objetos */
+
+  var number1 = '35';
+  /* var number2 = '75'; */
+  /* alert('mi numero favorito es '+number1); */
+  // las únicas elementos que deben estar en una variable son//
+  // numeros, letras, guion bajos, y signo $
+  // deben empezar con una letra
+  // estos son CaseSensitive lo que significa que si leen minúsculas y mayusculas
+
+  // hay 
+
+  // Arrays
+ // estos estan hechos para guardar grupos de valores
+
+ /* var Colores = ['Azul', 'Rojo', 'Verde']; */
+ /* var Colores = new Array('Rojo', 'Azul', 'Verde');
+ Colores.push('Violeta'); */
+  /* var Numeros = [3, 56, 77, 35]; */
+ /* alert(Numeros[0] + Numeros[3]);  */
+
+ /* for(var i = 0; i <= 20; i++){
+     console.log(i);
+ }  */
+/*  var i = 0;
+ while(i < 10){
+    console.log(i);
+    i++;
+ } */
+ 
+
+
+ 
