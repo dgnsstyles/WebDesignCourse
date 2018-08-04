848b67b1248694f39498af6de947ed19a3b88c45
David Goren <davidgoren@MacBook-Pro-de-David.local>
2018-08-03 23:44:59 -0400

    agregado la landing page

---
+++ Landing/main.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin:'bottom'
+});
+sr.reveal('.showcase-left', {
+  duration: 2000,
+  origin:'top',
+  distance:'300px'
+});
+sr.reveal('.showcase-right', {
+  duration: 2000,
+  origin:'right',
+  distance:'300px'
+});
+sr.reveal('.showcase-btn', {
+  duration: 2000,
+  delay: 2000,
+  origin:'bottom'
+});
+sr.reveal('#testimonial div', {
+  duration: 2000,
+  origin:'bottom'
+});
+sr.reveal('.info-left', {
+  duration: 2000,
+  origin:'left',
+  distance:'300px',
+  viewFactor: 0.2
+});
+sr.reveal('.info-right', {
+  duration: 2000,
+  origin:'right',
+  distance:'300px',
+  viewFactor: 0.2
+});
+sr.reveal('.parallax-text', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+})
+
+$(function() {
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset(200).top
+          }, 4000);
+          return false;
+        }
+      }
+    });
+  });
